2d04fb8246c7870ab81016dbda5a7881bbcc43b3
sho3la <mohamed_sh3lan61@yahoo.com>
2020-05-04 00:26:54 +0200

    QlineEdit

---
+++ 03_QLineEdit/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+
+
+class Window : public QWidget
+{
+
+public:
+	Window(QWidget* parent = nullptr) : QWidget(parent)
+	{
+		setFixedSize(500,250);
+
+		name_edit = new QLineEdit(this);
+		name_edit->move(100,100);
+		name_edit->setStyleSheet("background : red; font : bold 15px; color : white;");
+
+		name = new QLabel("Name", this);
+		name->move(50,100);
+	}
+
+private:
+	QLabel* name;
+	QLineEdit* name_edit;
+};
+
+int main(int argc, char* argv[])
+{
+	QApplication app(argc,argv);
+	Window window;
+
+	window.show();
+
+	return app.exec();
+}
+
